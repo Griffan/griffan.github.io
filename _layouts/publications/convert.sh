a5332ba3a49b57b5a68f29c4740ef2f1684f0447
griffan <zhangfanqw@gmail.com>
2018-12-05 18:37:05 -0500

    remove title bibliography

--- _layouts/publications/convert.sh
+++ _layouts/publications/convert.sh
@@ -1,2 +1,2 @@
 pandoc --filter=pandoc-citeproc --standalone mybib.md -o mybib.html --metadata pagetitle="mybib"
-perl -e '$flag=0;while(<>){if(/body/){$flag ^= 1;next;}print if $flag;}' mybib.html |cat ../header.html - ../foot.html >../publications.html
+perl -e '$flag=0;while(<>){if(/body/){$flag ^= 1;next;}print if $flag;}' mybib.html |grep -v "Bibliography"|cat ../header.html - ../foot.html >../publications.html
