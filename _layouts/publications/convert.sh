c5d5e9f251572a3c2088ffc222c89d18fb02380d
griffan <zhangfanqw@gmail.com>
2018-12-04 20:36:47 -0500

    adjusted bib

--- _layouts/publications/convert.sh
+++ _layouts/publications/convert.sh
@@ -1,1 +1,2 @@
 pandoc --filter=pandoc-citeproc --standalone mybib.md -o mybib.html --metadata pagetitle="mybib"
+perl -e '$flag=0;while(<>){if(/body/){$flag ^= 1;next;}print if $flag;}' mybib.html |cat ../header.html - ../foot.html >../publications.html
